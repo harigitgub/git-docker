9e7b1305486bafa135de363f0281715890380363
harigitgub <harimidleware@gmail.com>
2018-02-05 21:09:06 +0530

    checking for launching instance

---
+++ ec2-launch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ami=ami-1ee65166
+if ["$ami" ==  "ami-1ee65166"]
+then
+aws ec2 run-instances --image-id  $ami --count 1 --instance-type t2.micro --key-name new-key --region us-west-2
+else
+echo " The only supported ami is ami-1ee65166 and region is us-west-2"
+exit 1
+fi
