2c0de1de6da7bfbd47481e0e2c399c5d14062924
harigitgub <harimidleware@gmail.com>
2018-02-05 21:15:48 +0530

    Update ec2-launch.sh

--- ec2-launch.sh
+++ ec2-launch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ami = ami-1ee65166
+
 if ["$ami" ==  "ami-1ee60166"]
 then
 aws ec2 run-instances --image-id  $ami --count 1 --instance-type t2.micro --key-name new-key --region us-west-2
